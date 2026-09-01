2fefef65d8ddd96b91eede44c34635f23923f3cc
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2026-08-31 23:10:10 -0400

    fix companion build failure

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -11,6 +11,10 @@ const workspaceRoot = path.resolve(projectRoot, '..');
 const config = getDefaultConfig(projectRoot);
 
 config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
 
 const portalEntry = require.resolve('@rn-primitives/portal');
 const baseResolveRequest = config.resolver.resolveRequest;
